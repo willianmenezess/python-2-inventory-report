5bb2085c33d8472b89d36097e16fc649c33e63db
willianmenezess <willmenezes@outlook.com.br>
2023-10-31 17:28:19 -0300

    feat: resolve requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,4 +1,26 @@
-def test_create_product() -> None:
-    raise NotImplementedError
+from inventory_report.product import Product
 
-# primeiro commit
+
+def test_create_product() -> None:
+    # criando um dicionário com os parâmetros do construtor da classe Product
+    params = {
+        "id": "1",
+        "product_name": "Arroz",
+        "company_name": "Marca",
+        "manufacturing_date": "10/10/2020",
+        "expiration_date": "10/10/2021",
+        "serial_number": "123456",
+        "storage_instructions": "Manter em local seco e arejado.",
+    }
+    # **params expande o dicionário params em argumentos nomeados
+    instance_product = Product(**params)
+    assert instance_product.id == "1"
+    assert instance_product.product_name == "Arroz"
+    assert instance_product.company_name == "Marca"
+    assert instance_product.manufacturing_date == "10/10/2020"
+    assert instance_product.expiration_date == "10/10/2021"
+    assert instance_product.serial_number == "123456"
+    assert (
+        instance_product.storage_instructions
+        == "Manter em local seco e arejado."
+    )
