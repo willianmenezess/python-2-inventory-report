382cce4c1fc372e9066efbeb9561dc39394959cc
willianmenezess <willmenezes@outlook.com.br>
2023-11-01 10:46:31 -0300

    feat: resolve requisito 5

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -24,7 +24,7 @@ class JsonImporter(Importer):
         return list_products
 
 
-class CsvImporter:
+class CsvImporter(Importer):
     pass
 
 
--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,15 @@
+from inventory_report.product import Product
+from typing import Optional
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[list[Product]] = None):
+        self.__data: list[Product] = data if data is not None else []
+
+    def add_data(self, data: list[Product]) -> None:
+        self.__data.extend(data)
+
+    # Sem o @property, teria que chamar o método como uma função:
+    @property
+    def data(self) -> list[Product]:
+        return self.__data
